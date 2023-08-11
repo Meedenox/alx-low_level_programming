e02c3eb1ea4abb474455780be022215a130d15e6
Meedenox <mohammed.ennahli@gmail.com>
2023-08-10 17:39:16 -0700

    new edit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
-
+/*
+ * main - Entry point
+ *
+ * Description: A C program
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
-// This is the main function, the entry point of the program.
 {
 puts("Programming is like building a multilingual puzzle");
 return (0);
