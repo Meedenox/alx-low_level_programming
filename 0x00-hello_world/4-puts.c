532907b7a49c60021da9506cf037305602ea2fcb
Meedenox <mohammed.ennahli@gmail.com>
2023-08-10 17:13:47 -0700

    to the 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,7 @@
-#!/bin/bash
-puts ("Programming is like building a multilingual puzzle,\n); return 0 | gcc file.c
+#include <stdio.h>
+
+int main(void)
+{
+    puts("Programming is like building a multilingual puzzle,");
+    return 0
+}
