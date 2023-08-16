9d6e77ea53e5a1882c5d07cd402d20f328001793
Meedenox <mohammed.ennahli@gmail.com>
2023-08-15 18:30:07 -0700

    edut to 11 rhird time

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,20 +1,20 @@
-#include "main.h"
+#include"main.h"
 
 /**
- * print_to_98
+ * print_to_98 - print n to 98 counts
  *
  * @n: takes number input
 */
 
 void print_to_98(int n)
-{       
+{
         int count;
-        
+
         if (n > 98)
                 for (count = n; count > 98; count--)
-                        printf("%d, ",count);
+                        printf("%d, ", count);
         else
                 for (count = n; count < 98; count++)
-                        printf("%d, ",count);
+                        printf("%d, ", count);
         printf("98\n");
 }
