b5c6690b69975f9ca136313a1789976bc64ea5ed
Meedenox <mohammed.ennahli@gmail.com>
2023-08-15 19:00:50 -0700

    11 again

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,12 +8,13 @@
 
 void print_to_98(int n)
 {
-int count;
-if (n > 98)
-for (count = n; count > 98; count--)
-printf("%d, ", count);
-else
-for (count = n; count < 98; count++)
-prinf("%d, ", count);
-print("98\n");
+        int count;
+
+        if (n > 98)
+                for (count = n; count > 98; count--)
+                        printf("%d, ", count);
+        else
+                for (count = n; count < 98; count++)
+                        printf("%d, ", count);
+        printf("98\n");
 }
