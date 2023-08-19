09b25a5b7a18a177128eff534e5dfcdcc1b95eb1
Meedenox <mohammed.ennahli@gmail.com>
2023-08-18 18:17:38 -0700

    edit 4 and add prototype to main.h

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *print_most_numbers -check the code
+ *
+ *
+ *return: void
+ */
+void print_most_numbers(void)
+
+{
+	char c;
+
+	for (c = '0'; c != '4'; c != '2'; c <= '9')
+	{
+	_putchar(c);
+	}
+	_putchar("\n");
+
+
+}
