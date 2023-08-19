8ce5c3f1efee4076e104aec10be278835707baf5
Meedenox <mohammed.ennahli@gmail.com>
2023-08-19 02:58:46 -0700

    edit 6 and add prototype to header

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *
+ *print_line- check the cod
+ *
+ *return: void
+ *
+ */
+
+void print_line(int n)
+
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		putchar('_');
+	}
+	_putchar('\n');
+}
