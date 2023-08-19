8230fd267aa7e918bb118ed35948e23ed3a8c46e
Meedenox <mohammed.ennahli@gmail.com>
2023-08-18 18:24:46 -0700

    edit 4 second

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -10,11 +10,10 @@ void print_most_numbers(void)
 {
 	char c;
 
-	for (c = '0'; c != '4'; c != '2'; c <= '9'; c++)
+	for (c = '0'; c <= '9'; c++)
 	{
-	_putchar(c);
+		if (c != '4'; c != '2')
+			_putchar(c);
 	}
 	_putchar("\n");
-
-
 }
