64df7453f21ce29e98a648aa778db11bfd1b8d24
Meedenox <mohammed.ennahli@gmail.com>
2023-08-18 18:27:18 -0700

    edit 4 the 3

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -6,14 +6,14 @@
  *return: void
  */
 void print_most_numbers(void)
-
 {
+	
 	char c;
 
 	for (c = '0'; c <= '9'; c++)
 	{
-		if (c != '4'; c != '2')
-			_putchar(c);
+		if (!(c == '2' || c == '4'))
+		_putchar(c);
 	}
-	_putchar("\n");
+	_putchar('\n');
 }
