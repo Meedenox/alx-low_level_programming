95413888ddb404021d027841562175ed805c072c
Meedenox <mohammed.ennahli@gmail.com>
2023-08-20 08:01:06 -0700

    anew edit to 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -9,12 +9,15 @@
 void print_line(int n)
 {
 	if (n <= 0)
+	{
 		_putchar('\n');
+	}
 	else
 	{
-		for (i = 0; i < n; i++)
-
-		putchar('_');
+		for (int i = 0; i < n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
 	}
-
 }
