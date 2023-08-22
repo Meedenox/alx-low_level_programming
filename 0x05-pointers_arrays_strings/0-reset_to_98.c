c0a91e5ddf02abf462c1dfb74a0d5b97b2574d08
Meedenox <mohammed.ennahli@gmail.com>
2023-08-21 19:05:03 -0700

    edit to 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,8 @@
 #include "main.h"
 /**
- *
- *reset_to_98- chek code
+ *reset_to_98 - chek code
  *description : updating the value of a pointer
- *
+ *@n: is the name of pointer
  *return: 0
  *
  */
