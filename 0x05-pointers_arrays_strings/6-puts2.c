c7fa7bdf196310de588a2efc33607a02f29a2412
Meedenox <mohammed.ennahli@gmail.com>
2023-08-22 19:25:45 -0700

    6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *puts2 - check the code
+ *@str: A pointer to an int that will be changed
+ *
+ *Return: void 
+ */
+
+void puts2(char *str)
+{
+	int a;
+
+	for (a = 0; str[a] != '\0'; a++)
+	{
+		if (a % 2 == 0)
+			_putchar (str[a]);
+	}
+	_putchar ('\n');
+}
