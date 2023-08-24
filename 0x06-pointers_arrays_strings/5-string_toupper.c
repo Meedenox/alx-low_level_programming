220a59d131ab1722ec9358fb0ca180dd6d86f586
Meedenox <mohammed.ennahli@gmail.com>
2023-08-24 14:12:12 -0700

    5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * *string_toupper -function that changes  lowercase to uppercase
+ *
+ * @str: string of return
+ * Return: str
+ *
+ *
+ *
+ */
+char *string_toupper(char *str)
+{
+	int i;
+
+	for (int i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] = str[i] - 32;
+		}
+	}
+	return (str);
+}
