7af7617996d259c1fcfcbefc9685c3708da0078d
Meedenox <mohammed.ennahli@gmail.com>
2023-08-24 06:12:42 -0700

    0 edit

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,9 +2,9 @@
 /**
  *_strcat -concatenates two strings
  *
- *
- *
- *return: the resulting string
+ *@dest:the first string
+ *@src: the second string
+ *Return: the resulting string
  *
  *
  */
@@ -13,9 +13,9 @@ char *_strcat(char *dest, char *src)
 {
 	int n1, n2;
 
-	while dest[n1]
+	while (dest[n1])
 		n1++;
-	for (n2 = 0, src[n2] <= "\0", n2++)
+	for (n2 = 0; src[n2]; n2++)
 		dest[n1++] = src[n2];
-	return (dest[n1++])
+	return (dest);
 }
