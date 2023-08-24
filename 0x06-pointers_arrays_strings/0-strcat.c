63f76775cc3aaf3066df073d29b4e0007f42cbc6
Meedenox <mohammed.ennahli@gmail.com>
2023-08-24 06:00:43 -0700

    0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *_strcat -concatenates two strings
+ *
+ *
+ *
+ *return: the resulting string 
+ *
+ *
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int n1 , n2;
+	while dest[n1]
+		n1++;
+	for (n2=0,src[n2] <= "\0",n2++)
+		dest[n1++]=src[n2];
+	return dest[n1++]
+}
