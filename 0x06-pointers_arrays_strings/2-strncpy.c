2d949d5bf1a8260d91a2cfbb4ccda3440b98e1f8
Meedenox <mohammed.ennahli@gmail.com>
2023-08-24 10:17:32 -0700

    2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,7 +11,6 @@ char *_strncpy(char *dest, char *src, int n)
 	for (i = 0; i < n && src[i] != '\0'; i++)
 		dest[i] = src[i];
 	while (i < n)
-		dest[i] = '\0';
-	i++
+		dest[i++] = '\0';
 		return (dest);
 }
