17930d0941cf132e2870c9120a9a729ff5239f33
Meedenox <mohammed.ennahli@gmail.com>
2023-08-29 06:07:01 -0700

    0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -8,11 +8,10 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i;
 
-	for (i = 0; n > 0 ; i++, n--)
+	for (unsigned int i = 0; n > 0 ; i++, n--)
 	{
-		s[i] = b;
+		s[i] = 'b';
 	}
 
 	return (s)
