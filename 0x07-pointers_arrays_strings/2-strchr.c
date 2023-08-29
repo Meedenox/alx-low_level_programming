560c934710113a48f58adc7879b09c137166a1aa
Meedenox <mohammed.ennahli@gmail.com>
2023-08-29 09:10:02 -0700

    2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * *_strchr - function that locates a character in a string.
+ *
+ * @s: astring of character
+ * @c: a character
+ *
+ * Return: null or c
+ *
+ *
+ *
+ */
+char *_strchr(char *s, char c)
+{
+	unsigned int i;
+
+	for (i = 0; s[i] >=  '\0'; i++)
+	{
+		if (s[i] == 'c')
+		{
+			return (c);
+		}
+
+	}
+		return (NULL);
+}
+
+}
