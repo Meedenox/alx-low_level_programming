a0e58a7438c700e58616e817fb9b75a69861015b
Meedenox <mohammed.ennahli@gmail.com>
2023-08-31 03:56:52 -0700

    1

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -8,11 +8,10 @@
 
 void _print_rev_recursion(char *s)
 {
-	int n, i;
-
-	for (i = (n - 1) ; s[n] == '\0')
-		_putchar(s[i]);
-	_print_rev_recursion(s[i - 1])
-
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
 
 }
