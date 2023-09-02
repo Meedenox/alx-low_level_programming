297d41203ed7980f5f738533e1dafa69b3457f74
Meedenox <mohammed.ennahli@gmail.com>
2023-09-02 10:57:32 -0700

    new etdit to 100

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -12,17 +12,16 @@
 int _atoi(char *s)
 {
 	unsigned int num = 0;
-	int sign =1;
-	do
-	{
+	int sign = 1;
+
+	do {
 		if (*s == '-')
 			sign *= -1;
 		else if (*s >= '0' && *s <= '9')
 			num = (num * 10) + (*s - '0');
 		else if (num > 0)
-		       	break;
-	} 
-	while (*s++);
+			break;
+	} while (*s++);
 
-	return (num * sign);
+	return (num * sign);
 }
