39a82a24f42fd3e87e20eb0b527db029c53f3ae7
Meedenox <mohammed.ennahli@gmail.com>
2023-09-02 14:42:00 -0700

    new dir

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *main -function that print the programe name 
+ *@argv: list
+ *@argc: int
+ *Return: 0
+ *
+ */
+int main(int argc, char const *argv[])
+{
+
+	(void)argc;
+
+	printf("%s\n", argv[0]);
+	return(0);
+}
