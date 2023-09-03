c6d717589c9bd0437e028ab92d36b23bd6889577
Meedenox <mohammed.ennahli@gmail.com>
2023-09-02 17:43:19 -0700

    new era

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,7 +1,13 @@
 #include "main.h"
+/**
+ *main- function that print
+ *Return:void
+ */
 
 int main(void)
 {
-    _puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
-    return (0);
+	_puts("\"At the end of the day, my goal was to be the best hacker\"\n");
+	_puts("\t- Kevin Mitnick");
+
+	return (0);
 }
